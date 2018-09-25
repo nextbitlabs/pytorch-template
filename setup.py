7fb20c779fd82f94a5a46f41046773479af74996
Luca Grementieri <lgrementieri@nextb.it>
2018-09-25 10:27:30 +0200

    add package information

---
+++ setup.py
@@ -0,0 +1,61 @@
+import os
+import re
+from codecs import open
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pytorch-template',
+    version=find_version('pytorch-template', '__init__.py'),
+    description='PyTorch code and documentation template',  #  TODO: update
+    long_description=long_description,
+    author='Nextbit AI Team',
+    author_email='lgrementieri@nextbit.it',  #  TODO: update
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Data scientists',  #  TODO: update
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+
+        # Pick your license as you wish
+        'License :: Proprietary license',
+
+        # Specify the Python versions you support here.
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    keywords='AI deep learning pytorch template',  #  TODO: update
+    packages=find_packages(exclude=['build', 'data', 'dist', 'docs', 'tests']),
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy >= 1.13',
+        'torch >= 0.4.1',
+        'torchvision >= 0.2.1',
+        'tqdm >= 4.23'
+    ]
+)
