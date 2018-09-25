3856f3967ff3a5b8a67c5512e9e08ee28136757d
Luca Grementieri <lgrementieri@nextb.it>
2018-09-25 17:44:15 +0200

    improve dataset definition

--- pytorch_template/ingestion/datasets.py
+++ pytorch_template/ingestion/datasets.py
@@ -7,7 +7,6 @@ from torch.utils.data import Dataset
 
 
 class IngestDataset(Dataset):
-    ACCEPTED_EXTENSIONS = ('npy',)  # TODO: update
 
     def __init__(self,
                  root_dir: str,
@@ -16,23 +15,18 @@ class IngestDataset(Dataset):
                  transform: Optional[Callable[[np.array], np.array]] = None):
         self.root_dir = os.path.expanduser(os.path.normpath(root_dir))
         self.split = split
-        self.targets = pd.read_csv(os.path.join(self.root_dir, targets_file), index_col=0)
+        self.dataframe = pd.read_csv(os.path.join(self.root_dir, targets_file), index_col=0)
         self.transform = transform
 
-        split_path = os.path.join(self.root_dir, self.split)
-        self.filepaths = tuple(sorted(
-            os.path.join(self.split, f) for f in os.listdir(split_path) if
-            f.rsplit('.', 1)[1].lower() in NpyDataset.ACCEPTED_EXTENSIONS))
-
     def __len__(self) -> int:
-        return len(self.filepaths)
+        return len(self.dataframe)
 
     # TODO: update
     def __getitem__(self,
                     idx: int) -> Tuple[np.array, float, str]:  # TODO: update return types
-        filepath = os.path.join(self.root_dir, self.filepaths[idx])
+        filepath = os.path.join(self.root_dir, self.dataframe.index[idx])
         features = np.load(filepath)
-        target = self.targets.loc[self.filepaths[idx]]['target']
+        target = self.dataframe.iloc[idx, 0]
         filename = os.path.basename(filepath).rsplit('.', 1)[0]
 
         sample = (features, target, filename)
