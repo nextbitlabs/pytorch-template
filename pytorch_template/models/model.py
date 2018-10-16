b1b21d6a1d07d91f8ccc0ff2899f0d6bf91d459b
Luca Grementieri <lgrementieri@nextb.it>
2018-10-16 14:24:28 +0000

    Feature/bugfix

--- pytorch_template/models/model.py
+++ pytorch_template/models/model.py
@@ -15,6 +15,23 @@ from ..utils.monitors import Monitor
 class Model:
     SUMMARY_STEPS = 10  # TODO: update
 
+    @staticmethod
+    def _get_optimizer_info(optimizer: torch.optim.Optimizer) -> str:
+        info = type(optimizer).__name__
+        for attr in optimizer.defaults:
+            info = info + " | {}: {}".format(attr, optimizer.defaults[attr])
+        return info
+
+    # noinspection PyProtectedMember
+    @staticmethod
+    def _get_scheduler_info(scheduler: torch.optim.lr_scheduler._LRScheduler) -> str:
+        attr_to_exclude = ('optimizer', 'is_better')
+        info = type(scheduler).__name__
+        for attr in scheduler.__dict__:
+            if hasattr(scheduler, attr) and attr not in attr_to_exclude:
+                info = info + " | {}: {}".format(attr, scheduler.__dict__[attr])
+        return info
+
     def __init__(self,
                  module: nn.Module):
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -35,10 +52,12 @@ class Model:
 
         # TODO: update optimizer
         optimizer = torch.optim.SGD(self.module.parameters(), lr=lr,
-                                    momentum=0.9, nesterov=True)
+                                    momentum=0.9, nesterov=True, weight_decay=1e-4)
+        logging.info('Optimizer: {}'.format(self._get_optimizer_info(optimizer)))
+
         # TODO: update scheduler
-        scheduler = torch.optim.lr_scheduler.StepLR(
-            optimizer, step_size=epochs // 3, gamma=0.8)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=epochs // 3, gamma=0.8)
+        logging.info('Scheduler: {}'.format(self._get_scheduler_info(scheduler)))
 
         total_step = 0
         val_log_string = 'Validation after epoch {:d} - Loss: {:.4f} - L1: {:.4f}'
